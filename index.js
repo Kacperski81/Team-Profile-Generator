666e0fc1b0894d6e073200d1958097e2e8d277db
Marcin Kacperski <kacperski81@gmail.com>
2023-01-28 20:26:50 +0000

    added error check to user input

--- index.js
+++ index.js
@@ -11,16 +11,47 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+function tryAgain() {
+    return inquirer.prompt([{
+        name: "repeat",
+        type: "confirm",
+        message: "Want to try again?"
+    }]).then(answer => {
+        if(answer.repeat) return inquirerPrompt(questions);
+        process.exit();
+    })
+}
+
+function errorCheck(answers) {
+    const {manager_name,manager_id,manager_email,office_number} = answers;
+    if(Number(manager_name) || !manager_name.length) {
+        console.log('Manager name should be a string.', manager_name.length);
+        return tryAgain();
+    }
+    if(!manager_id) {
+        console.log('Manager id should be a number');
+        return tryAgain();
+    }
+    if(Number(manager_email) || !manager_email.length) {
+        console.log('Manager emial should be a string');
+        return tryAgain();
+    }
+    if(!office_number) {
+        console.log('Office number should be number');
+        return tryAgain();
+    }
+    return answers;
+}
+
+function inquirerPrompt(questions) {
+    return inquirer.prompt(questions)
+    .then(answers => errorCheck(answers))
+    // .then(() => tryAgain());
+}
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 function init() {
-    inquirer.prompt(questions).then(answer => {
-        if(typeof answer.manager_name !== 'string' || !answer.manager_name) {
-            console.log('Please try again');
-            return inquirer.prompt.questions
-        };
-        
-    }
-)}
+    return inquirerPrompt(questions);
+}
 
 init();
