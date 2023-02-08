7403eaa95c7bc285433fe2d0f0a92c1c15f9422a
Marcin Kacperski <kacperski81@gmail.com>
2023-02-08 14:20:25 +0000

    page generated

--- index.js
+++ index.js
@@ -13,34 +13,14 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./src/page-template.js");
 const { template } = require("lodash");
 
-function tryAgain() {
-    return inquirer.prompt([{
-        name: "repeat",
-        type: "confirm",
-        message: "Want to try again?"
-    }]).then(answer => {
-        if(answer.repeat) return inquirerPrompt(questions);
-        process.exit();
-    })
+function writeToFile(outputPath, data) {
+    // console.log(data);
+    console.log(outputPath)
+    // console.log(text);
+    fs.writeFile(outputPath,data,(err) => {
+        if(err) return console.log(err);
+    });
 }
-
-function errorCheck(answers) {
-    const {manager_name,manager_id,manager_email,office_number} = answers;
-    if(Number(manager_name) || !manager_name.length) {
-        throw new Error('Manager name should be a string.');
-    }
-    if(!manager_id) {
-        throw new Error('Manager id should be a number');
-    }
-    if(Number(manager_email) || !manager_email.length) {
-        throw new Error('Manager emial should be a string');
-    }
-    if(!office_number) {
-        throw new Error('Office number should be number');
-    }
-    return answers;
-}
-
 
 function addAnotherTeamMember({team_member_type},teamMembers) {
     switch(team_member_type) {
@@ -57,20 +37,20 @@ function addAnotherTeamMember({team_member_type},teamMembers) {
                 .then(answers => addAnotherTeamMember(answers,teamMembers));
             })
         default :
-            console.log('all should be added',teamMembers);
+            writeToFile(outputPath,render(teamMembers));
             return;
         }
 }
 
 function questionsForNewIntern(teamMembers) {
     return inquirer.prompt(internQuestions).then(answers => {
-        teamMembers.push(answers)
+        teamMembers.push(new Intern(answers.intern_name,answers.intern_id,answers.intern_email,answers.intern_School))
         return addAnotherTeamMember(answers,teamMembers)});
 }
 
 function questionsForNewEngineer(teamMembers) {
     return inquirer.prompt(engineerQuestions).then(answers => {
-        teamMembers.push(answers)
+        teamMembers.push(new Engineer(answers.engineer_name,answers.engineer_id,answers.engineer_email,answers.engineer_gitHub))
         return addAnotherTeamMember(answers,teamMembers)});
 }
 
@@ -81,23 +61,11 @@ function askUserWhoHeWantToAdd() {
 function inquirerPrompt(questions) {
     return inquirer.prompt(questions)
     .then(answers => {
-        teamMembers.push(answers)
+        teamMembers.push(new Manager(answers.manager_name, answers.manager_id, answers.manager_email, answers.office_number))
         return askUserWhoHeWantToAdd(answers,teamMembers)})
-    .then(answers => addAnotherTeamMember(answers,teamMembers))};
-    // .then(answers => {
-    //     if(answers.team_member_type === 'Engineer') {
-    //         console.log(answers.team_member_type);
-    //         questionsForNewEngineer().then(answers => {
-    //             return askUserWhoHeWantToAdd().then(answers => addAnotherTeamMember(answers));
-    //         });
-    //     }
-    //     if(answers.team_member_type === 'Intern') {
-    //         console.log(answers.team_member_type);
-    //         questionsForNewIntern().then(answers => {
-    //             return askUserWhoHeWantToAdd().then(answers => addAnotherTeamMember(answers));
-    //         });
-    //     }
-    // });
+    .then(answers => addAnotherTeamMember(answers,teamMembers))
+}
+
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 function init() {
     console.log('Please build your team');
